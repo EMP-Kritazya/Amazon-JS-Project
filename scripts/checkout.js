00d3ad6d92365c29e5c77d7154a9afd3ea34ab0c
EMP-Kritazya <emp.kritazya@gmail.com>
2026-02-10 18:17:39 -0600

    Regenrate HTML when updating delivery option

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -85,176 +85,177 @@ function renderItemsSummary() {
     `;
   });
   document.querySelector(".js-left").innerHTML = cartItemsHtml;
+
+  // Updates the cart quantity in the Header of the page
+  updateHeaderCartQuantity();
+  // Updates total cost of all items in the cart
+  updateTotal();
+  // Updates Order Summary tile
+  updateOrderSummary();
+
+  function deliveryOptionsHTML(matchingProduct, cartItem) {
+    let html = "";
+    deliveryOptions.forEach((deliveryOption) => {
+      const today = dayjs();
+      const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
+      const dateString = deliveryDate.format("dddd, MMMM D");
+      const priceString =
+        deliveryOption.priceCents === 0
+          ? "FREE"
+          : `$${deliveryOption.priceCents / 100} -`;
+      const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
+
+      html += `
+        <div class="delivery-option-buttons js-delivery-option js-edit-delivery-option-${matchingProduct.id}" data-delivery-option-id = "${deliveryOption.id}" data-product-id= "${matchingProduct.id}">
+          <input
+            ${isChecked ? "checked" : ""}
+            type="radio"
+            name="delivery-option-1-${matchingProduct.id}"
+            class="delivery-option-input"
+          />
+          <div>
+            <div class="delivery-option-date">${dateString}</div>
+            <div class="delivery-option-price">${priceString} Shipping</div>
+          </div>
+        </div>
+      `;
+    });
+    return html;
+  }
+
+  // Listens for click on DELETE link for all items and functions accordingly
+  document.querySelectorAll(".js-delete-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      document.querySelector(`.js-cart-item-container-${productId}`).remove();
+
+      calculateCartQuantity();
+      updateTotal();
+      updateHeaderCartQuantity();
+    });
+  });
+
+  // Listens for click on UPDATE link for all items and functions accordingly
+  document.querySelectorAll(".js-update-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+
+      // I need to display the selector and Save link
+      document
+        .querySelector(`.js-cart-details-${productId}`)
+        .classList.add("is-editing-quantity", "is-not-editable");
+    });
+  });
+
+  // Listens for click on SAVE link for all items and functions accordingly
+  document.querySelectorAll(".js-save-link").forEach((link) => {
+    link.addEventListener("click", () => {
+      const productId = link.dataset.productId;
+
+      // I need to get value from the input box and update cart quantity for that Item
+      const newQuantity = parseInt(
+        document.querySelector(`.js-input-quantity-${productId}`).value,
+      );
+
+      // Revert back to showing update and delete links and also updates the item quantity
+      document
+        .querySelector(`.js-cart-details-${productId}`)
+        .classList.remove("is-editing-quantity", "is-not-editable");
+
+      updateCart(productId, newQuantity);
+
+      updateTotal();
+
+      updateCartQuantity(productId);
+    });
+  });
+
+  // Listens to click on Delivery Dates
+  document.querySelectorAll(`.js-delivery-option`).forEach((element) => {
+    element.addEventListener("click", () => {
+      const { productId, deliveryOptionId } = element.dataset;
+
+      // Updates the deliveryId for cart
+      updateDeliveryOptions(productId, deliveryOptionId);
+      renderItemsSummary();
+    });
+  });
+
+  function updateOrderSummary() {
+    totalBeforeTax = totalItemsCost + shippingHandling;
+    orderHtml = `
+    <div class="payment-summary">
+      <div class="payment-title">Order Summary</div>
+      <div class="payment-details">
+        <div class="elaborate-details">
+          <div class="items">
+            <div class="items-title">Items (${totalCartItems}):</div>
+            <div class="item-price">$${(totalItemsCost / 100).toFixed(2)}</div>
+          </div>
+          <div class="shipping">
+            <div class="shipping-title">Shipping & handling:</div>
+            <div class="shipping-price">$${shippingHandling.toFixed(2)}</div>
+          </div>
+          <div class="tax-details">
+            <div class="before-tax">
+              <div class="total-before-tax">Total before tax:</div>
+              <div class="total-before-tax-price">$${(totalBeforeTax / 100).toFixed(2)}</div>
+            </div>
+            <div class="actual-tax">
+              <div class="estimated-title">Estimated tax (10%):</div>
+              <div class="estimated-amount">$${((totalBeforeTax * 0.1) / 100).toFixed(2)}</div>
+            </div>
+          </div>
+        </div>
+        <div class="final-price">
+          <div class="order-total-title">Order total:</div>
+          <div class="total-price">$${((totalBeforeTax + totalBeforeTax * 0.1) / 100).toFixed(2)}</div>
+        </div>
+      </div>
+      <button class="place-order">Place your order</button>
+    </div>
+  `;
+    document.querySelector(".js-right").innerHTML = orderHtml;
+  }
+
+  // Display total cart size in the header
+  function updateHeaderCartQuantity() {
+    calculateCartQuantity();
+    document.querySelector(".return-to-home-link").innerHTML =
+      `${totalCartItems} items`;
+  }
+
+  // Updates totalCost for all items
+  function updateTotal() {
+    // reset variables
+    totalItemsCost = 0;
+
+    // update variables
+    cart.forEach((cartItem) => {
+      let matchingItem = products.find(
+        (item) => item.id === cartItem.productId,
+      );
+      totalItemsCost += cartItem.quantity * matchingItem.priceCents;
+    });
+    updateOrderSummary();
+  }
+
+  // Update cart item quantity after updating number of items
+  function updateCartQuantity(productId) {
+    const product = cart.find((item) => item.productId === productId);
+    const quantity = product.quantity;
+
+    document.querySelector(`.js-quantity-${productId}`).innerHTML = quantity;
+    updateHeaderCartQuantity();
+  }
+
+  function getDeliveryDate(deliveryOptionId) {
+    const today = dayjs();
+    const deliveryDate = today.add(parseInt(deliveryOptionId), "days");
+    const dateString = deliveryDate.format("dddd, MMMM D");
+
+    return dateString;
+  }
 }
 renderItemsSummary();
-
-// Updates the cart quantity in the Header of the page
-updateHeaderCartQuantity();
-// Updates total cost of all items in the cart
-updateTotal();
-// Updates Order Summary tile
-updateOrderSummary();
-
-function deliveryOptionsHTML(matchingProduct, cartItem) {
-  let html = "";
-  deliveryOptions.forEach((deliveryOption) => {
-    const today = dayjs();
-    const deliveryDate = today.add(deliveryOption.deliveryDays, "days");
-    const dateString = deliveryDate.format("dddd, MMMM D");
-    const priceString =
-      deliveryOption.priceCents === 0
-        ? "FREE"
-        : `$${deliveryOption.priceCents / 100} -`;
-    const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
-
-    html += `
-      <div class="delivery-option-buttons js-delivery-option js-edit-delivery-option-${matchingProduct.id}" data-delivery-option-id = "${deliveryOption.id}" data-product-id= "${matchingProduct.id}">
-        <input
-          ${isChecked ? "checked" : ""}
-          type="radio"
-          name="delivery-option-1-${matchingProduct.id}"
-          class="delivery-option-input"
-        />
-        <div>
-          <div class="delivery-option-date">${dateString}</div>
-          <div class="delivery-option-price">${priceString} Shipping</div>
-        </div>
-      </div>
-    `;
-  });
-  return html;
-}
-
-// Listens for click on DELETE link for all items and functions accordingly
-document.querySelectorAll(".js-delete-link").forEach((link) => {
-  link.addEventListener("click", () => {
-    const productId = link.dataset.productId;
-    removeFromCart(productId);
-    document.querySelector(`.js-cart-item-container-${productId}`).remove();
-
-    calculateCartQuantity();
-    updateTotal();
-    updateHeaderCartQuantity();
-  });
-});
-
-// Listens for click on UPDATE link for all items and functions accordingly
-document.querySelectorAll(".js-update-link").forEach((link) => {
-  link.addEventListener("click", () => {
-    const productId = link.dataset.productId;
-
-    // I need to display the selector and Save link
-    document
-      .querySelector(`.js-cart-details-${productId}`)
-      .classList.add("is-editing-quantity", "is-not-editable");
-  });
-});
-
-// Listens for click on SAVE link for all items and functions accordingly
-document.querySelectorAll(".js-save-link").forEach((link) => {
-  link.addEventListener("click", () => {
-    const productId = link.dataset.productId;
-
-    // I need to get value from the input box and update cart quantity for that Item
-    const newQuantity = parseInt(
-      document.querySelector(`.js-input-quantity-${productId}`).value,
-    );
-
-    // Revert back to showing update and delete links and also updates the item quantity
-    document
-      .querySelector(`.js-cart-details-${productId}`)
-      .classList.remove("is-editing-quantity", "is-not-editable");
-
-    updateCart(productId, newQuantity);
-
-    updateTotal();
-
-    updateCartQuantity(productId);
-  });
-});
-
-// Listens to click on Delivery Dates
-document.querySelectorAll(`.js-delivery-option`).forEach((element) => {
-  element.addEventListener("click", () => {
-    const { productId, deliveryOptionId } = element.dataset;
-
-    // Updates the deliveryId for cart
-    updateDeliveryOptions(productId, deliveryOptionId);
-
-    renderItemsSummary();
-  });
-});
-
-function updateOrderSummary() {
-  totalBeforeTax = totalItemsCost + shippingHandling;
-  orderHtml = `
-  <div class="payment-summary">
-    <div class="payment-title">Order Summary</div>
-    <div class="payment-details">
-      <div class="elaborate-details">
-        <div class="items">
-          <div class="items-title">Items (${totalCartItems}):</div>
-          <div class="item-price">$${(totalItemsCost / 100).toFixed(2)}</div>
-        </div>
-        <div class="shipping">
-          <div class="shipping-title">Shipping & handling:</div>
-          <div class="shipping-price">$${shippingHandling.toFixed(2)}</div>
-        </div>
-        <div class="tax-details">
-          <div class="before-tax">
-            <div class="total-before-tax">Total before tax:</div>
-            <div class="total-before-tax-price">$${(totalBeforeTax / 100).toFixed(2)}</div>
-          </div>
-          <div class="actual-tax">
-            <div class="estimated-title">Estimated tax (10%):</div>
-            <div class="estimated-amount">$${((totalBeforeTax * 0.1) / 100).toFixed(2)}</div>
-          </div>
-        </div>
-      </div>
-      <div class="final-price">
-        <div class="order-total-title">Order total:</div>
-        <div class="total-price">$${((totalBeforeTax + totalBeforeTax * 0.1) / 100).toFixed(2)}</div>
-      </div>
-    </div>
-    <button class="place-order">Place your order</button>
-  </div>
-`;
-  document.querySelector(".js-right").innerHTML = orderHtml;
-}
-
-// Display total cart size in the header
-function updateHeaderCartQuantity() {
-  calculateCartQuantity();
-  document.querySelector(".return-to-home-link").innerHTML =
-    `${totalCartItems} items`;
-}
-
-// Updates totalCost for all items
-function updateTotal() {
-  // reset variables
-  totalItemsCost = 0;
-
-  // update variables
-  cart.forEach((cartItem) => {
-    let matchingItem = products.find((item) => item.id === cartItem.productId);
-    totalItemsCost += cartItem.quantity * matchingItem.priceCents;
-  });
-  updateOrderSummary();
-}
-
-// Update cart item quantity after updating number of items
-function updateCartQuantity(productId) {
-  const product = cart.find((item) => item.productId === productId);
-  const quantity = product.quantity;
-
-  document.querySelector(`.js-quantity-${productId}`).innerHTML = quantity;
-  updateHeaderCartQuantity();
-}
-
-function getDeliveryDate(deliveryOptionId) {
-  const today = dayjs();
-  const deliveryDate = today.add(parseInt(deliveryOptionId), "days");
-  const dateString = deliveryDate.format("dddd, MMMM D");
-
-  return dateString;
-}
