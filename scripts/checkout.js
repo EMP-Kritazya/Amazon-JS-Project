fb0af8116b2ce5757ed10c35024e83220c048e0d
EMP-Kritazya <emp.kritazya@gmail.com>
2026-02-14 06:53:15 -0600

    Convert call-back code to promises

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,7 +2,11 @@ import { renderItemsSummary } from "./checkout/itemSummary.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { loadProducts } from "../data/product.js";
 
-loadProducts(() => {
+new Promise((resolve) => {
+  loadProducts(() => {
+    resolve();
+  });
+}).then(() => {
   renderItemsSummary();
   renderOrderSummary();
 });
