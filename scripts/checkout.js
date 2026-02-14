21f707c0b67d747fd49af2fd9adfea93c9dd530f
EMP-Kritazya <emp.kritazya@gmail.com>
2026-02-14 08:42:19 -0600

    Convert from promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,9 +2,16 @@ import { renderItemsSummary } from "./checkout/itemSummary.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { loadProducts, loadProductsFetch } from "../data/product.js";
 
-new Promise((resolve) => {
-  loadProductsFetch().then(() => {
-    renderItemsSummary();
-    renderOrderSummary();
-  });
-});
+async function loadPage() {
+  await loadProductsFetch();
+  renderItemsSummary();
+  renderOrderSummary();
+}
+loadPage();
+
+// new Promise((resolve) => {
+//   loadProductsFetch().then(() => {
+//     renderItemsSummary();
+//     renderOrderSummary();
+//   });
+// });
